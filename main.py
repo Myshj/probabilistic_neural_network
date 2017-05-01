e4284507654c32afa7814e11e623794eafef34d7
Myshj <donmarin@mail.ru>
2017-05-01 10:36:24 +0300

    Добавлен радиус функции Гаусса.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ with open('data.json') as data_file:
 inputs = data_dict['inputs']
 weights_to_image_layer = data_dict['weights_to_image_layer']
 count_of_neurons_in_image_layer = data_dict['count_of_neurons_in_image_layer']
+radius_of_gauss_function = data_dict['radius_of_gauss_function']
 
 y = {}
 theta = {}
@@ -20,7 +21,7 @@ for image in count_of_neurons_in_image_layer.keys():
         for input_neuron in range(0, len(inputs)):
             t = (weights_to_image_layer[input_neuron][image][image_neuron] - inputs[input_neuron])
             weighted_sum += math.exp(
-                -t * t
+                -t * t / radius_of_gauss_function
             )
         y[image].append(weighted_sum)
 
