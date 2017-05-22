dc7990b78478f91c73fe13dbbe737b2b03d37340
Myshj <donmarin@mail.ru>
2017-05-22 19:26:55 +0300

    Добавлены комментарии в код.

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import json
 import math
 
 
+# открываем файл с исходными данными
 with open('data.json') as data_file:
     data_dict = json.load(data_file)
 
-
+# читаем исходные данные
 inputs = data_dict['inputs']
 weights_to_image_layer = data_dict['weights_to_image_layer']
 count_of_neurons_in_image_layer = data_dict['count_of_neurons_in_image_layer']
 radius_of_gauss_function = data_dict['radius_of_gauss_function']
 
+# для хранения выхода слоёв
 y = {}
 theta = {}
 
+# считаем выходы слоя образов
 for image in count_of_neurons_in_image_layer.keys():
     y[image] = []
     for image_neuron in range(0, count_of_neurons_in_image_layer[image]):
@@ -25,6 +28,7 @@ for image in count_of_neurons_in_image_layer.keys():
             )
         y[image].append(weighted_sum)
 
+# считаем выходы слоя сложения
 for image in count_of_neurons_in_image_layer.keys():
     image_sum = 0.0
     for image_neuron in range(0, count_of_neurons_in_image_layer[image]):
@@ -32,8 +36,10 @@ for image in count_of_neurons_in_image_layer.keys():
     image_sum /= count_of_neurons_in_image_layer[image]
     theta[image] = image_sum
 
+# определяем наибольший выход сети
 best_result = max(theta.values())
 
+# определяем классы, которым соответствует лучший выход сети
 for k in theta.keys():
     if theta[k] == best_result:
         print(k)
